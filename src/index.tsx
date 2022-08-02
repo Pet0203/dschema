ceac8009d9a48f454a845c360e907223057e7e88
Gnusson.net <admin@gnusson.net>
2022-08-02 11:16:02 +0200

    📦 NEW: Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './scss/global.scss';
+import App from './views/App/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
---
+++ src/views/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../scss/main.scss';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <p>TB Schema</p>
+    </div>
+  );
+}
+
+export default App;
