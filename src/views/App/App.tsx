f072e5172b6281cc001c199896ee0df3530c93d9
Gnusson.net <admin@gnusson.net>
2022-08-03 14:19:20 +0200

    👌 IMPROVE: Added icons and animated the heart

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Select from 'react-select';
 import '../../scss/main.scss';
 import styles from './App.module.scss';
+import { Clipboard, Gift, Heart } from 'react-feather';
 
 function App() {
   const [selectedOption, setSelectedOption] = useState({ value: 'grupp1', label: 'Grupp 1' });
@@ -23,7 +24,7 @@ function App() {
     <div className={styles.app}>
       <section className={styles.section}>
         <div className={styles.container}>
-          <h3 className={styles.title}>TB Schema | Prenumerera</h3>
+          <h2 className={styles.title}>TB Schema | Prenumerera</h2>
           <div>
             <h4 className={styles.subtitle}>Välj grupp</h4>
             <div className={styles.items}>
@@ -32,7 +33,7 @@ function App() {
           </div>
           <div>
             <h4 className={styles.subtitle}>Välj kurser</h4>
-            <div className={[styles.items, styles["items--checkboxes"]].join(' ')}>
+            <div className={[styles.items, styles['items--checkboxes']].join(' ')}>
               <div>
                 <label htmlFor="math">Matematik</label>
                 <input type="checkbox" id="math" name="math" value="Matematik"></input>
@@ -56,7 +57,7 @@ function App() {
           </div>
           <div className={styles.calendar_url}>
             <h4 className={styles.subtitle}>Kalender URL</h4>
-            <button className={styles.calendar_url__primary_button}>Kopiera kalender url</button>
+            <button className={styles.calendar_url__primary_button}><Clipboard size={16} /> Kopiera kalender url</button>
             <p>OR manually copy the url</p>
             <input className={styles.calendar_url__url_input} type="text" readOnly={true} value="https://testurl.comdnuanwduanwdunauiwdniauwnduawndiuanw" />
           </div>
@@ -68,9 +69,12 @@ function App() {
           <button>Ladda ner .ical</button> */}
 
           <div className={styles.credits}>
-            <p>Made with ❤ in Gothenburg, Sweden</p>
+            <p className={styles.credits__made_with}>Made with<Heart className={styles["credits__made_with--heart"]} size={16} />in Gothenburg, Sweden</p>
             <p>En slant för arbetet skadar aldrig!</p>
-            <a href="https://paypal.me/memgod">PayPal</a>
+            <a className={styles.credits__paypal} href="https://paypal.me/memgod">
+              <Gift size={16} />
+              PayPal
+            </a>
           </div>
         </div>
       </section>
