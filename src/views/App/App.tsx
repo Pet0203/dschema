50e32f587d601052dacc5ca4a04204ed37f98aa9
Gnusson.net <admin@gnusson.net>
2022-08-04 10:48:53 +0200

    👌 IMPROVE: Multistep setup for better UX

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -4,55 +4,21 @@ import '../../scss/main.scss';
 import styles from './App.module.scss';
 import { Clipboard, Gift, Heart } from 'react-feather';
 
+interface IGroups {
+  value: string;
+  label: string;
+}
+
+interface ICourses {
+  value: string;
+  label: string;
+}
+
 function App() {
-  /**
-   * @type
-   * {value: string, label: string}
-   * @value
-   * Is returned from the select component when a value is selected
-   * @label
-   * Is the text that is displayed in the select component
-   * @description
-   * This is the object that is the current selected value in the select component
-   * @example
-   * {value: 'grupp1', label: 'Grupp 1'}
-   */
-  const [selectedGroup, setSelectedGroup] = useState({ value: 'grupp1', label: 'Grupp 1' });
+  const [selectedGroup, setSelectedGroup] = useState<IGroups>();
+  const [selectedCourses, setSelectedCourses] = useState<ICourses[]>();
 
-  /**
-   * @type
-   * [{value: string, label: string}]
-   * @value
-   * Is returned from the select component when a value is selected
-   * @label
-   * Is the text that is displayed in the select component
-   * @description
-   * This is the object that is the current selected value in the select component
-   * @example
-   * [{value: 'matte', label: 'Matte'}]
-   */
-  const [selectedCourses, setSelectedCourses] = useState([
-    { value: 'matte', label: 'Matte' },
-    { value: 'fysik', label: 'Fysik' },
-    { value: 'kemi', label: 'Kemi' },
-    { value: 'proFys', label: 'Projektkurs Fysik' },
-    { value: 'proKem', label: 'Projektkurs Kemi' },
-    { value: 'programmering', label: 'Programmering' },
-  ]);
-
-  /**
-   * @type
-   * [{value: string, label: string}]
-   * @value
-   * Is returned from the select component when a value is selected
-   * @label
-   * Is the text that is displayed in the select component
-   * @description
-   * This is the array of selected options for the select component.
-   * @example
-   * {value: 'grupp1', label: 'Grupp 1'}
-   */
-  const groups = [
+  const groups: IGroups[] = [
     { value: 'grupp1', label: 'Grupp 1' },
     { value: 'grupp2', label: 'Grupp 2' },
     { value: 'grupp3', label: 'Grupp 3' },
@@ -65,18 +31,6 @@ function App() {
     { value: 'grupp10', label: 'Grupp 10' },
   ];
 
-  /**
-   * @type
-   * [{value: string, label: string}]
-   * @value
-   * Is returned from the select component when a value is selected
-   * @label
-   * Is the text that is displayed in the select component
-   * @description
-   * This is the array of selected options for the select component.
-   * @example
-   * [{value: 'matte', label: 'Matte'}]
-   */
   const courses = [
     { value: 'matte', label: 'Matte' },
     { value: 'fysik', label: 'Fysik' },
@@ -85,6 +39,14 @@ function App() {
     { value: 'proKem', label: 'Projektkurs Kemi' },
     { value: 'programmering', label: 'Programmering' },
   ];
+
+  const handleGroupChange = (selected?: IGroups | IGroups[] | null) => {
+    setSelectedGroup(selected as IGroups);
+  };
+
+  const handleCoursesChange = (selected: readonly ICourses[]) => {
+    setSelectedCourses(selected as ICourses[]);
+  };
 
   return (
     <div className={styles.app}>
@@ -96,41 +58,47 @@ function App() {
               Välj grupp
             </h4>
             <div className={styles.items}>
-              <Select className={styles.react_select_container} defaultValue={selectedGroup} options={groups} />
+              <Select className={styles.react_select_container} placeholder="Välj undergrupp" defaultValue={selectedGroup} onChange={handleGroupChange} options={groups} />
             </div>
           </div>
-          <div>
-            <h4 className={[styles.subtitle, styles.tooltip].join(' ')} data-tooltip="Välj de kurser du vill inkludera i schemat">
-              Välj kurser
-            </h4>
-            <div className={styles.items}>
-              <Select className={styles.react_select_container} isMulti closeMenuOnSelect={false} defaultValue={selectedCourses} options={courses} />
-            </div>
-          </div>
-          <div>
-            <h4 className={[styles.subtitle, styles.tooltip].join(' ')} data-tooltip='Lägger till "Lindholmen" eller "Johanneberg" i platsfältet" '>
-              Modifikationer
-            </h4>
-            <div className={styles.items}>
-              <label className={styles.checkbox}>
-                Förbättra platsfältet
-                <input name="mod1" type="checkbox"></input>
-                <span className={styles.checkbox__checkmark}></span>
-              </label>
-            </div>
-          </div>
-          <div className={styles.calendar_url}>
-            <h4 className={[styles.subtitle, styles.tooltip].join(' ')} data-tooltip="Brukar gå att klistra in i de flesta kalender apparna">
-              Kalender URL
-            </h4>
-            <div className={[styles.items, styles['items--calendar_url']].join(' ')}>
-              <button className={styles.calendar_url__primary_button}>
-                <Clipboard size={16} /> Kopiera kalender url
-              </button>
-              <p>OR manually copy the url</p>
-              <input className={styles.calendar_url__url_input} type="text" readOnly={true} value="https://testurl.comdnuanwduanwdunauiwdniauwnduawndiuanw" />
-            </div>
-          </div>
+          {selectedGroup && selectedGroup.value && (
+            <>
+              <div>
+                <h4 className={[styles.subtitle, styles.tooltip].join(' ')} data-tooltip="Välj de kurser du vill inkludera i schemat">
+                  Välj kurser
+                </h4>
+                <div className={styles.items}>
+                  <Select className={styles.react_select_container} isMulti closeMenuOnSelect={false} defaultValue={selectedCourses} onChange={handleCoursesChange} options={courses} />
+                </div>
+              </div>
+              <div>
+                <h4 className={[styles.subtitle, styles.tooltip].join(' ')} data-tooltip='Lägger till "Lindholmen" eller "Johanneberg" i platsfältet" '>
+                  Modifikationer
+                </h4>
+                <div className={styles.items}>
+                  <label className={styles.checkbox}>
+                    Förbättra platsfältet
+                    <input name="mod1" type="checkbox"></input>
+                    <span className={styles.checkbox__checkmark}></span>
+                  </label>
+                </div>
+              </div>
+              {selectedCourses && selectedCourses.length !== 0 && (
+                <div className={styles.calendar_url}>
+                  <h4 className={[styles.subtitle, styles.tooltip].join(' ')} data-tooltip="Brukar gå att klistra in i de flesta kalender apparna">
+                    Kalender URL
+                  </h4>
+                  <div className={[styles.items, styles['items--calendar_url']].join(' ')}>
+                    <button className={styles.calendar_url__primary_button}>
+                      <Clipboard size={16} /> Kopiera kalender url
+                    </button>
+                    <p>OR manually copy the url</p>
+                    <input className={styles.calendar_url__url_input} type="text" readOnly={true} value="https://testurl.comdnuanwduanwdunauiwdniauwnduawndiuanw" />
+                  </div>
+                </div>
+              )}
+            </>
+          )}
           <div className={styles.credits}>
             <p className={styles.credits__made_with}>
               Made with
