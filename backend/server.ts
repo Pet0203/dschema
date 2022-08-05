95ab05074d46c6886cd75f5e4c32c094c68ee561
Gnusson.net <admin@gnusson.net>
2022-08-05 08:53:05 +0200

    👌 IMPROVE: Declared and commented all req props

--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,18 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/api/v1/getUrl/', (req: any, res: any) => {
+  // Object with below properties
   const request = req.body;
+  
+  // String of group
+  const group = req.body.group;
+  // Boolean of whether or not to improve the location
+  const modLocation = req.body.modLocation;
+  // Boolean of whether or not to show exams in calendar
+  const modExam = req.body.modExam;
+  // Array of courses
+  const courses = req.body.courses;
+
   // Do your stuff and return the result as json format
   // ex. res.send({ url: 'https://tbschema.......' });
   res.send(request);
