4f6c4e18e4ecc45b2b441d1885a5f454cfc36eee
GnussonNet <admin@gnusson.net>
2023-11-01 01:18:21 +0100

    Redesigned using NextUi

---
+++ client/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+
+const { nextui } = require("@nextui-org/react");
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              foreground: "#000",
+              DEFAULT: "#fa6607",
+            },
+          },
+        },
+        dark: {
+          colors: {
+            primary: {
+              foreground: "#000",
+              DEFAULT: "#fa6607",
+            },
+          },
+        },
+      },
+    }),
+  ],
+};
